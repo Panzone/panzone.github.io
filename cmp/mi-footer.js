351e6753f4d10a023fe423ad7c874fd59fa6a4ed
Panzone <86033150+Panzone@users.noreply.github.com>
2021-06-26 20:33:10 -0500

    Delete mi-footer.js

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement {
-  connectedCallback(){
-    thi.innerHTML = /* html */
-      'Copyright &copy; 2021 Aguilera Ortega Jordy Ariel.';
-  }
-}
-customElements.define("mi-footer",MiFooter);
