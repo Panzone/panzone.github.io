f757eb690a7c40ed9c06e7f7ef1ffb4008b0821a
Panzone <86033150+Panzone@users.noreply.github.com>
2021-06-26 20:34:49 -0500

    Add files via upload

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,1 +1,13 @@
+class MiFooter
+  extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `<p>
+        &copy; 2021
+        Aguilera Ortega Jordy Ariel.
+      </p>`;
+  }
+}
 
+customElements.define(
+  "mi-footer", MiFooter);
